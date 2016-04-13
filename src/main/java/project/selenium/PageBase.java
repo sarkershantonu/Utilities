f5c69512892af3517a22c7a79b287d2e5bd90845
sarkershantonu <shantonu_oxford@yahoo.com>
2016-04-13 23:58:01 +0200

    updated

--- src/main/java/project/selenium/PageBase.java
+++ src/main/java/project/selenium/PageBase.java
@@ -45,6 +45,5 @@ public abstract class PageBase {
         return this;
     }
 
-
 }
 
--- src/main/java/project/selenium/pages/HomePage.java
+++ src/main/java/project/selenium/pages/HomePage.java
@@ -6,5 +6,8 @@ import project.selenium.PageBase;
 public class HomePage extends PageBase{
 
 
-
+    @Override
+    public void setPageLoadText(String pageLoadText) {
+        // todo
+    }
 }
