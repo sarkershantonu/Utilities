c46fa2ed30b7bb2dfbb09678d2452de626876fbb
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-05 23:27:59 +0200

    test updates

--- src/test/java/automation/suits/Acceptance/UseCase_01.java
+++ src/test/java/automation/suits/Acceptance/UseCase_01.java
@@ -4,4 +4,5 @@ package automation.suits.Acceptance;
  * Created by shantonu on 5/5/16.
  */
 public class UseCase_01 {
+    public void accptance_01_NameOf(){}
 }
--- src/test/java/automation/suits/performance/NevigationTiming.java
+++ src/test/java/automation/suits/performance/NevigationTiming.java
@@ -4,4 +4,6 @@ package automation.suits.performance;
  * Created by shantonu on 5/5/16.
  */
 public class NevigationTiming {
+    void homePageTiming(){}
+    void productPageTiming(){}
 }
