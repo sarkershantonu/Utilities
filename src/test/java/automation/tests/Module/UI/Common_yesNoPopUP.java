c20b3dbaa8eafef71d134e685486d590e77bef2f
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-05 23:13:18 +0200

    added blank test cases to apply structure

---
+++ src/test/java/automation/tests/Module/UI/Common_yesNoPopUP.java
@@ -0,0 +1,7 @@
+package automation.tests.Module.UI;
+
+/**
+ * Created by shantonu on 5/5/16.
+ */
+public class Common_yesNoPopUP {
+}
---
+++ src/test/java/automation/tests/Module/thirdParty/SocialMedia_twitter.java
@@ -0,0 +1,7 @@
+package automation.tests.Module.thirdParty;
+
+/**
+ * Created by shantonu on 5/5/16.
+ */
+public class SocialMedia_twitter {
+}
