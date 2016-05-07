273a8214471982f2de6517b5360cee314c71e2c4
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-07 18:06:18 +0200

    Updated

--- src/test/java/automation/tests/unit/LogInPage.java
+++ src/test/java/automation/tests/unit/LogInPage.java
@@ -1,7 +1,14 @@
 package automation.tests.unit;
 
+import org.junit.BeforeClass;
+import ru.yandex.qatools.allure.annotations.Title;
+
 /**
  * Created by shantonu on 5/5/16.
  */
 public class LogInPage {
+    @BeforeClass
+    public static void  initClass(){
+
+    }
 }
