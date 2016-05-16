e6085a870d36e2fea543660fd8fa906aaaee897e
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-17 00:28:30 +0200

    added items

--- src/main/java/project/selenium/pages/common/TopBar.java
+++ src/main/java/project/selenium/pages/common/TopBar.java
@@ -1,11 +1,15 @@
 package project.selenium.pages.common;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import project.selenium.PageBase;
 
 /**
  * Created by shantonu on 5/16/16.
  */
-public class TopBar
+public interface TopBar
 {
+    @FindBy(xpath = "")//property loading or after parsing or static
+    WebElement searchButton = null;
 
 }
