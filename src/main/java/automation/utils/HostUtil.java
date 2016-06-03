ca7b83575928125b6e76acb86627b8e8843399ff
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-03 23:46:18 +0200

    Host info util added

--- src/main/java/automation/utils/HostUtil.java
+++ src/main/java/automation/utils/HostUtil.java
@@ -1,7 +1,17 @@
 package automation.utils;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Created by shantonu on 6/3/16.
  */
 public class HostUtil {
+    public static String getHostName(){
+        try {
+            return InetAddress.getLocalHost().getCanonicalHostName();
+        } catch (UnknownHostException e) {
+            return e.getMessage();
+        }
+    }
 }
