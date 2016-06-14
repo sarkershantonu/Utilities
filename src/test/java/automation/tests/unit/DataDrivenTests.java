402c5fed3002fca90b5e0bd37a8ff76f7bd5707d
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-15 01:40:21 +0200

    support added

--- src/test/java/automation/tests/unit/DataDrivenTests.java
+++ src/test/java/automation/tests/unit/DataDrivenTests.java
@@ -1,7 +1,21 @@
 package automation.tests.unit;
 
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.loader.LoaderType;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
+import org.junit.runner.RunWith;
+
 /**
  * Created by shantonu on 6/15/16.
+ * todo, an full example for tests
  */
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(loaderType = LoaderType.CSV, filePaths = {"path to csv "})
 public class DataDrivenTests {
+
+}
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(loaderType = LoaderType.EXCEL, filePaths = {"path to excel "})
+class DataTests {
+
 }
