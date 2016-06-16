e1ab155cc141c45b6c2cbd4629cec916ad4e0dd9
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-17 00:24:57 +0200

    easy excel demo test cases

--- src/test/java/automation/tests/unit/DataDrivenTests.java
+++ src/test/java/automation/tests/unit/DataDrivenTests.java
@@ -3,19 +3,37 @@ package automation.tests.unit;
 import org.easetech.easytest.annotation.DataLoader;
 import org.easetech.easytest.loader.LoaderType;
 import org.easetech.easytest.runner.DataDrivenTestRunner;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * Created by shantonu on 6/15/16.
- * todo, an full example for tests
+ *
  */
 @RunWith(DataDrivenTestRunner.class)
-@DataLoader(loaderType = LoaderType.CSV, filePaths = {"path to csv "})
+
 public class DataDrivenTests {
 
-}
-@RunWith(DataDrivenTestRunner.class)
-@DataLoader(loaderType = LoaderType.EXCEL, filePaths = {"path to excel "})
-class DataTests {
+    @Test
+    @DataLoader(loaderType = LoaderType.CSV, filePaths = {"data/"})
+    public void testCsvInputs(){
+
+    }
+
+    @Test
+    @DataLoader(loaderType = LoaderType.EXCEL, filePaths = {"data/ "})
+    public void testExcelInputs(){
+
+    }
+    @Test
+    @DataLoader(loaderType = LoaderType.XML, filePaths = {"data/ "})
+    public void testXMLInputs(){
+
+    }
+    @Test
+    @DataLoader(loaderType = LoaderType.CUSTOM, filePaths = {"data/ "})
+    public void testCustomInputs(){
+
+    }
 
 }
