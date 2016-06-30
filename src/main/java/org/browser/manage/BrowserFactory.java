77210a58da34abd546af5f6debf28502cec86035
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-01 01:37:19 +0200

    chrome error solved

--- src/main/java/org/browser/manage/BrowserFactory.java
+++ src/main/java/org/browser/manage/BrowserFactory.java
@@ -4,6 +4,7 @@ import automation.utils.PropertyUtil;
 import org.browser.utils.RemoteConfig;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -112,12 +113,35 @@ public class BrowserFactory {
         else if("ie".equals(browserName)){
             //File iedriver = new File(getIEPath()); // reading from property //-Dwebdriver.ie.driver=physicall using command line
             File iedriver = new File("Point your Selenium Server exe Path");//todo for your PC
-            System.setProperty("webdriver.ie.driver", iedriver.getAbsolutePath());
+            PropertyUtil.setSystemProperty("webdriver.ie.driver", iedriver.getAbsolutePath());
 
             aBrowser = new InternetExplorerDriver();
         }
         else if("chrome".equals(browserName)){
-            aBrowser = new ChromeDriver();
+
+            String pathWindows = "C:\\Users\\%USERNAME%\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe";// can be changed for your PC
+            String pathlunix = "/usr/local/bin/chromedriver";
+            ChromeDriverService service ;
+            if (os.contains("windows")) {
+                PropertyUtil.setSystemProperty("webdriver.chrome.driver",pathWindows);
+                service = new ChromeDriverService.Builder()
+                        .usingDriverExecutable(new File(pathlunix))
+                        .usingAnyFreePort()
+                        .build();
+            } else {
+                PropertyUtil.setSystemProperty("webdriver.chrome.driver", pathlunix);
+                service = new ChromeDriverService.Builder()
+                        .usingDriverExecutable(new File(pathlunix))
+                        .usingAnyFreePort()
+                        .build();
+            }
+            try {
+                service.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
+
         }
         else if("safari".equals(browserName)){
             aBrowser = new SafariDriver();
