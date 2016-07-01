0f8f65d839d4f7d3de2741eadde18ca1c88ced10
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-01 02:25:06 +0200

    managble hub, todo

--- src/main/java/org/browser/manage/remote/SeleniumHub.java
+++ src/main/java/org/browser/manage/remote/SeleniumHub.java
@@ -1,7 +1,33 @@
 package org.browser.manage.remote;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.Semaphore;
+
 /**
  * Created by shantonu on 7/1/16.
+ *
+ * todo , implement a managable parallal system driven by remote drivers (local support also) 
  */
 public class SeleniumHub {
+    private Semaphore available = new Semaphore(1, true);
+
+    private Queue<WebDriver> drivers = new LinkedList<>();
+
+    private BlockingDeque<RemoteWebDriver> remoteWebDrivers;
+
+    private final boolean isReeusable;
+    private final boolean isRemote ;
+
+    public SeleniumHub(Queue<WebDriver> drivers,boolean isReeusable ,boolean isRemote) {
+        this.drivers = drivers;
+        this.isReeusable = isReeusable;
+        this.isRemote = isRemote;
+
+
+    }
 }
