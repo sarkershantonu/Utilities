a98ac9257e10b32fb30f6beccbd2f4e580f36c22
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-04 01:20:59 +0200

    complete hub util

--- src/main/java/org/browser/manage/remote/SeleniumHub.java
+++ src/main/java/org/browser/manage/remote/SeleniumHub.java
@@ -2,19 +2,23 @@ package org.browser.manage.remote;
 
 import org.browser.manage.BrowserFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.Semaphore;
 
 /**
  * Created by shantonu on 7/1/16.
- *
- * todo , implement a managable parallal system driven by remote drivers (local support also)
+ * <p>
+ *  just another parallel web driver manage util as hub. concurrent semaphore added.
  */
 public class SeleniumHub {
     private Semaphore available = new Semaphore(1, true);
@@ -23,46 +27,115 @@ public class SeleniumHub {
 
     private Queue<WebDriver> drivers = new LinkedList<>();
     private final LinkedList<RemoteWDConnector> remoteWDConnectors = new LinkedList<>();
-    private BlockingDeque<RemoteWebDriver> remoteWebDrivers;
+    private BlockingDeque<RemoteWebDriver> remoteWebDrivers = new LinkedBlockingDeque<>();
 
     private final boolean isReeusable;
-    private final boolean isRemote ;
+    private final boolean isRemote;
 
-    public SeleniumHub(Queue<WebDriver> drivers,boolean isReeusable ,boolean isRemote, int timeout, int ready) {
-        this.drivers = drivers;
+    public SeleniumHub(boolean isReeusable, boolean isRemote, int timeout, int ready, String... servers) {
         this.isReeusable = isReeusable;
         this.isRemote = isRemote;
         this.timeout = timeout;
-
-
-    }
-
-    public WebDriver init(){
-        try {
-            return BrowserFactory.getDefaultRemoteDriver();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        this.ready(ready);
+        for (final String server : servers) {
+            RemoteWDConnector connector = new RemoteWDConnector(server, this.remoteWebDrivers);
+            connector.start();
+            this.remoteWDConnectors.add(connector);
         }
     }
 
-    public void ready(int drivers){
-        synchronized (this){
+    private WebDriver init() {
+        WebDriver dr = null;
+        if (isRemote) {
+            while (dr == null && !Thread.currentThread().isInterrupted()) {
+                try {
+                    dr = remoteWebDrivers.take();
+                } catch (WebDriverException e) {
+                    if (e.getMessage().contains("session cannot find")) {
+                        try {
+                            Thread.currentThread().interrupt();
+                            dr = BrowserFactory.getDefaultRemoteDriver();
+                        } catch (MalformedURLException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        } else {
+            dr = BrowserFactory.getDefaultDriver();
+        }
+        return dr;
+    }
+
+    public void ready(int drivers) {
+        synchronized (this) {
             this.available.release(drivers);
         }
     }
-    public WebDriver getDriver(){
-        try{
+
+    public WebDriver getDriver() {
+        try {
             this.available.acquire();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        WebDriver driver  = null;
-        if(isReeusable){
-            driver =this.drivers.poll();
+        WebDriver driver = null;
+        if (isReeusable) {
+            driver = this.drivers.poll();
         }
-        if (driver.equals(null)){
+        if (driver.equals(null)) {
             driver = init();
         }
         return driver;
+    }
+
+    /***
+     * Release web driver
+     * Quit it
+     *
+     * @param driver
+     */
+    public void free(WebDriver driver) {
+        synchronized (this) {
+            if (isReeusable) {
+                this.drivers.add(driver);
+            } else {
+                driver.quit();
+            }
+            this.available.release();
+        }
+    }
+
+    public WebDriverWait wait(WebDriver driver) {
+        return new WebDriverWait(driver, timeout);
+    }
+
+    public void quit() {
+        quitAllConnecctors();
+        quitAllRemoteDrivers();
+        quitAllDrivers();
+    }
+
+    private void quitAllConnecctors() {
+        for (final RemoteWDConnector connector : this.remoteWDConnectors) {
+            connector.interrupt();
+        }
+    }
+
+    private void quitAllRemoteDrivers() {
+        WebDriver driver = null;
+        while ((driver = remoteWebDrivers.poll()) != null) {
+            driver.quit();
+        }
+    }
+
+    private void quitAllDrivers() {
+        WebDriver driver = null;
+        while ((driver = drivers.poll()) != null) {
+            driver.quit();
+        }
     }
 }
