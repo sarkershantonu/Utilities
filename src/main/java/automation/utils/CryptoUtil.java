7e83ee5ba8d1c2459753a2004cf844a60178cd67
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-09 13:55:17 +0200

    decoder added

--- src/main/java/automation/utils/CryptoUtil.java
+++ src/main/java/automation/utils/CryptoUtil.java
@@ -1,5 +1,7 @@
 package automation.utils;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 /**
@@ -8,8 +10,19 @@ import java.util.Base64;
 public class CryptoUtil {
 
     public static String encrypt(String item){
-        byte[] encrypted =  Base64.getEncoder().encode(item.getBytes());
+        byte[] encrypted =  Base64.getEncoder().encode(item.getBytes(StandardCharsets.ISO_8859_1));//default Charset
         return new String(encrypted);
     }
-
+    public static String encrypt(String item, Charset charset){
+        byte[] encrypted =  Base64.getEncoder().encode(item.getBytes(charset));
+        return new String(encrypted);
+    }
+    public static String dencrypt(String item){
+        byte[] encrypted =  Base64.getDecoder().decode(item);
+        return new String(encrypted);
+    }
+    public static String dencrypt(String item, Charset charset){
+        byte[] encrypted =  Base64.getDecoder().decode(item.getBytes(charset));
+        return new String(encrypted);
+    }
 }
