be4567c2d5399f99aaefdc0fb4ffca500d90ae73
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-15 22:28:33 +0200

    screenshot util added

--- Automation-Utilities/src/main/java/org/automation/utils/tracking/LogUtil.java
+++ Automation-Utilities/src/main/java/org/automation/utils/tracking/LogUtil.java
@@ -1,4 +1,4 @@
-package org.automation.utils.error;
+package org.automation.utils.tracking;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
---
+++ Opencart Selenium/src/main/java/org/automation/opencart/App.java
@@ -0,0 +1,17 @@
+package org.automation.opencart;
+
+/**
+ * Created by shantonu on 8/14/16.
+ */
+class App {
+    public App(String a){
+        super();
+        System.out.println(a);
+        return;
+    }
+
+    public static void main(String[] args) {
+        new App("Huaa");
+        System.out.println("dada");
+    }
+}
