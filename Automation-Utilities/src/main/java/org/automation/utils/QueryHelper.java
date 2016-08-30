7f85dbfbf908cf2429db3561b0a6a30b4959112e
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-08-30 17:10:33 +0200

    QueryHelpe

---
+++ Automation-Utilities/src/main/java/org/automation/utils/QueryHelper.java
@@ -0,0 +1,30 @@
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public final class QueryHelper {
+    private QueryHelper() {}
+
+    public static void closeResultSet(ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+    }
+
+    public static void closeStatement(Statement stmt) throws SQLException {
+        if (stmt != null) {
+            stmt.close();
+        }
+    }
+
+    public static void closeResultSetAndStatement(ResultSet rs, Statement stmt) throws SQLException {
+        try {
+            closeResultSet(rs);
+        } finally {
+            closeStatement(stmt);
+        }
+    }
+
+}
