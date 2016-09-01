0f55a91043d9078eaebf0fcf70a6642e6d38a17a
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-01 23:53:24 +0200

    web driver test context

--- Selenium-Utilities/src/main/java/org/automation/angular/extras/WebDriverNGContext.java
+++ Selenium-Utilities/src/main/java/org/automation/angular/extras/WebDriverNGContext.java
@@ -1,5 +1,6 @@
 package org.automation.angular.extras;
 
+import org.automation.angular.WebDriverNG;
 import org.hamcrest.Matcher;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,8 +9,15 @@ import org.openqa.selenium.lift.find.Finder;
 
 /**
  * Created by shantonu on 9/1/16.
+ * todo
  */
 public class WebDriverNGContext implements TestContext {
+    private WebDriverNG driverNG;
+
+    public WebDriverNGContext(WebDriverNG driver){
+        this.driverNG = driver;
+    }
+
     @Override
     public void goTo(String url) {
 
