1745bd824c745a80693e008f89c21f9157883e5c
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-02 00:06:47 +0200

    WebDriver Test Context done for angular

--- Selenium-Utilities/src/main/java/org/automation/angular/extras/WebDriverNGContext.java
+++ Selenium-Utilities/src/main/java/org/automation/angular/extras/WebDriverNGContext.java
@@ -8,11 +8,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.lift.TestContext;
 import org.openqa.selenium.lift.find.Finder;
-import org.openqa.selenium.support.ui.Clock;
-import org.openqa.selenium.support.ui.Sleeper;
-import org.openqa.selenium.support.ui.SystemClock;
+import org.openqa.selenium.support.ui.*;
 
 import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.lift.match.NumericalMatchers.atLeast;
 
@@ -48,9 +47,7 @@ public class WebDriverNGContext implements TestContext {
 
     @Override
     public void assertPresenceOf(Matcher<Integer> cardinalityConstraint, Finder<WebElement, WebDriver> finder) {
-        driverNG.waitForAngular();
         Collection<WebElement> foundElements = finder.findFrom(driverNG);
-
         if (!cardinalityConstraint.matches(foundElements.size())) {
             Description description = new StringDescription();
             description.appendText("\nExpected: ")
@@ -74,16 +71,46 @@ public class WebDriverNGContext implements TestContext {
         WebElement element = findOneElementTo("type into", finder);
         element.sendKeys(input);
     }
+    public void clickOnFirst(Finder<WebElement, WebDriver> finder) {
+
+        WebElement element = findFirstElementTo("click on", finder);
+        element.click();
+    }
 
     @Override
     public void clickOn(Finder<WebElement, WebDriver> finder) {
-        driverNG.waitForAngular();
         WebElement element = findOneElementTo("click on", finder);
         element.click();
     }
 
     @Override
-    public void waitFor(Finder<WebElement, WebDriver> finder, long timeout) {
+    public void waitFor(Finder<WebElement, WebDriver> finder, long timeMS) {
+        driverNG.waitForAngular();
+        final ExpectedCondition<Boolean> elementsDisplayedPredicate = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                driverNG.waitForAngular();
+                final Collection<WebElement> elements = finder.findFrom(driver);
+                for (WebElement webElement : elements) {
+                    if (webElement.isDisplayed()) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+
+        final long defaultSleepTimeoutMillis = FluentWait.FIVE_HUNDRED_MILLIS.in(TimeUnit.MILLISECONDS);
+        final long sleepTimeout = (timeMS > defaultSleepTimeoutMillis)
+                ? defaultSleepTimeoutMillis : timeMS / 2;
+
+        Wait<WebDriver> wait = new WebDriverWait(driverNG, clock, sleeper, millisToSeconds(timeMS),
+                sleepTimeout) {
+            @Override
+            public RuntimeException timeoutException(String message, Throwable lastException) {
+                throw new AssertionError("Element was not rendered within " + timeMS + "ms");
+            }
+        };
+        wait.until(elementsDisplayedPredicate);
 
     }
 
@@ -107,5 +134,24 @@ public class WebDriverNGContext implements TestContext {
 
     private void failWith(String message) throws AssertionError {
         throw new java.lang.AssertionError(message);
+    }
+
+    private static long millisToSeconds(final long timeoutMillis) {
+        return ceiling(((double) timeoutMillis) / 1000);
+    }
+
+    private static long ceiling(final double value) {
+        final long asLong = (long) value;
+        final int additional = value - asLong > 0 ? 1 : 0;
+        return asLong + additional;
+    }
+    private WebElement findFirstElementTo(String action, Finder<WebElement, WebDriver> finder) {
+        driverNG.waitForAngular();
+        Collection<WebElement> foundElements = finder.findFrom(driverNG);
+        if (foundElements.isEmpty()) {
+            failWith("could not find element to " + action);
+        }
+
+        return foundElements.iterator().next();
     }
 }
