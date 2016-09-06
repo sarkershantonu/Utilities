8a58394f47dccffae967687028624d1f70a7c21f
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-09-06 13:49:14 +0200

    WorkflowDataserviceConfiguration

---
+++ Automation-Utilities/src/main/java/org/automation/utils/activity/WorkflowDataserviceConfiguration.java
@@ -0,0 +1,29 @@
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WorkflowDataserviceConfiguration {
+    private static final String WF_DAO_DATA_SERVICE = "wfDaoDataService";
+
+    @Bean(name = "bookmasterDataService")
+    public BookmasterDataService bookmasterDataService() {
+        return new BookmasterDataService();
+    }
+
+    @Bean(name = "wfDataService")
+    public WFDataService wfDataService() {
+        return new WFDataService();
+    }
+
+    @Bean(name = WF_DAO_DATA_SERVICE)
+    public DataService getDataService(@Qualifier(DataServiceConfiguration.DB_DATA_SOURCE) DataSource dataSource) throws Exception {
+        final DataServiceConfiguration config = new DataServiceConfiguration(); 
+        return config.getDataService(dataSource, WfDataServiceEnum.class, "/wf-mappings.xml", "/wf-data-model.xml");
+    }
+
+}
