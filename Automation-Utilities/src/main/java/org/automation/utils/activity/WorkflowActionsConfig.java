f24b1678d62cdc210397313db4549aa4103d2c2c
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-09-06 13:26:39 +0200

    Workflow Actions Config added

---
+++ Automation-Utilities/src/main/java/org/automation/utils/activity/WorkflowActionsConfig.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class WorkflowActionsConfig {
+
+    @Bean
+    public ActionRepository actionRepository(WFDataService wfDataService, WF wf) {
+        final ActionRepository actionRepository = new SingleActionRepository(null);
+        return actionRepository;
+    }
+}
