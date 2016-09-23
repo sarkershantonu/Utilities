2e8da8dfe535d7360488a93e26acd9c7c4fb104c
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-09-23 19:38:25 +0200

    demo Jpa Configuration

---
+++ Automation-Utilities/src/main/java/org/automation/utils/jms/JpaConfiguration.java
@@ -0,0 +1,46 @@
+
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class JpaConfiguration {
+
+    public static final String DIALECT = "org.hibernate.dialect.Oracle10gDialect";
+    //
+    @Bean
+    public JpaVendorAdapter vendor() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.ORACLE);
+        adapter.setDatabasePlatform(DIALECT);
+        return adapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter vendor) {
+        LocalContainerEntityManagerFactoryBean result = new LocalContainerEntityManagerFactoryBean();
+        result.setPersistenceProvider(new HibernatePersistenceProvider());
+        result.setDataSource(ds);
+        result.setJpaVendorAdapter(vendor);
+        result.setPackagesToScan(JpaConfiguration.class.getPackage().getName());
+        return result;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean factory) {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(factory.getObject());
+        return manager;
+    }
+}
