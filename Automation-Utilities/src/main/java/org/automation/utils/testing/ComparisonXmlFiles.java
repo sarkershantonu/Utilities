4b1ad337cec1793f4c3ba5368ae5049fa2700997
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-09-23 20:00:40 +0200

    common Comparison Xml Files

---
+++ Automation-Utilities/src/main/java/org/automation/utils/testing/ComparisonXmlFiles.java
@@ -0,0 +1,26 @@
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ComparisonXmlFiles {
+
+
+    public static void compareXml(String actualResponse, String expectedResponse, String rms) throws SAXException, TransformerException, ParserConfigurationException, IOException {
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setNormalizeWhitespace(Boolean.TRUE);
+
+        String strExpected = XMLPreparation.getPreparedXml(expectedResponse);
+        String strActual = XMLPreparation.getPreparedXml(actualResponse);
+        DetailedDiff diff = new DetailedDiff(XMLUnit.compareXML(strExpected, strActual));
+        List<?> allDifferences = diff.getAllDifferences();
+        assertEquals("Differences for " + rms + " found: " + diff.toString(), 0, allDifferences.size());
+
+    }
+}
