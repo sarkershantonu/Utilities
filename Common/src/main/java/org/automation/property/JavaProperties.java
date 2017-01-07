5f8300c75435d7af3ed0efab9d4d54c63c747e05
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-06 23:08:39 -0500

    added java default system properties

---
+++ Common/src/main/java/org/automation/property/JavaProperties.java
@@ -0,0 +1,21 @@
+package org.automation.property;
+
+/**
+ * Created by shantonu on 1/6/17.
+ */
+public final class JavaProperties {
+    public static String FILE_SEPERATOR=System.getProperty("file.separator");
+    public static String CLASSPATH=System.getProperty("java.class.path");
+    public static String JAVA_HOME=System.getProperty("java.home");
+    public static String JAVA_VENDOR=System.getProperty("java.vendor");
+    public static String JAVA_VENDOR_URL=System.getProperty("java.vendor.url");
+    public static String JAVA_VERSION=System.getProperty("java.version");
+    public static String LINE_SEPERATOR=System.getProperty("line.separator");
+    public static String OS_ARCHITECTURE=System.getProperty("os.arch");
+    public static String OS_NAME=System.getProperty("os.name");
+    public static String OS_VERSION=System.getProperty("os.version");
+    public static String JAVA_PATH_SEPERATOR=System.getProperty("path.separator");
+    public static String USER_WORKING_DIR=System.getProperty("user.dir");
+    public static String USER_HOME=System.getProperty("user.home");
+    public static String USER_NAME=System.getProperty("user.name");
+}
