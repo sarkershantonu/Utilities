3cc10060ddb29a48b72cd56a505e2758d05d38b0
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-06 23:13:39 -0500

    result set jdbc , todo

--- JDBC Utilities/src/main/java/org/automation/db/ResultSetUtils.java
+++ JDBC Utilities/src/main/java/org/automation/db/ResultSetUtils.java
@@ -1,7 +1,15 @@
 package org.automation.db;
 
+import java.sql.ResultSet;
+import java.util.List;
+
 /**
  * Created by shantonu on 12/31/16.
+ * todo
  */
 public class ResultSetUtils {
+
+    public static List<List<String>> getResultsAsTable (ResultSet resultSet){
+        return null;
+    }
 }
