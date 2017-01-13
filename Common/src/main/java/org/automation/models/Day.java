468b5706382ad289bb8191ec02c792050093c6d6
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-01-13 01:14:52 -0500

    day enum

---
+++ Common/src/main/java/org/automation/models/Day.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Day {
+	SUNDAY(1), MONDAY(2), TUESDAY(3), WEDNESDAY(4), THURSDAY(5), FRIDAY(6), SATURDAY(7);
+
+	private int value;
+
+	private Day(int value) {
+		this.value = value;
+		DayLookupMap.lookupMap.put(value, this);
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public static Day get(int value) {
+		return DayLookupMap.lookupMap.get(value);
+	}
+
+	private static final class DayLookupMap {
+		private static final Map<Integer, Day> lookupMap = new HashMap<>();
+	}
+}
