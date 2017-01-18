0f983a2e0e3a4cd5c9a6c23e1670a3e641d4ba19
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-01-17 23:14:05 -0500

    getAllResutlsInTable

--- JDBC Utilities/src/main/java/org/automation/db/ResultSetUtils.java
+++ JDBC Utilities/src/main/java/org/automation/db/ResultSetUtils.java
@@ -1,16 +1,33 @@
 package org.automation.db;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by shantonu on 12/31/16.
- * todo
+ * 
  */
 public class ResultSetUtils {
 
-    public static List<List<String>> getResultsAsTable (ResultSet resultSet){
-        return null;
+  public static List<List<String>> getAllResutlsInTable(final ResultSet resultSet) throws SQLException {
+        final List<List<String>> table = new ArrayList<>();
+        ResultSetMetaData rsmd = resultSet.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        while (resultSet.next()) {
+            List<String> aRow = new ArrayList<>();
+            for (int i = 1; i <= columnsNumber; i++) {
+
+                //String columnValue = resultSet.getString(i);
+
+                //System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                aRow.add(resultSet.getString(i));
+            }
+            System.out.println("");
+        }
+        return table;
     }
 
 
