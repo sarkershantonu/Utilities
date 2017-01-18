0473be5ac142bd8882f03fd22de884806aaef1cc
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-01-17 23:14:19 -0500

    printAllItemInResultSet

--- JDBC Utilities/src/main/java/org/automation/db/ResultSetUtils.java
+++ JDBC Utilities/src/main/java/org/automation/db/ResultSetUtils.java
@@ -30,5 +30,16 @@ public class ResultSetUtils {
         return table;
     }
 
-
+ public static void printAllItemInResultSet(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData rsmd = resultSet.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        while (resultSet.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = resultSet.getString(i);
+                System.out.print(columnValue + " " + rsmd.getColumnName(i));
+            }
+            System.out.println("");
+        }
+    }
 }
