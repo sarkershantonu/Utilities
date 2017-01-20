2f1e6d1c94b9a70d2ec1c81e7bc563c206e1289a
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-01-19 19:47:46 -0500

    Day Enum

---
+++ Data Utilities/src/main/java/org/automation/Day.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Day {
+	Sunday(1), Monday(2), Tuesday(3), Wednesday(4), Thursday(5), Friday(6), Saturday(7);
+
+	private int value;
+
+	private Day(int value) {
+		this.value = value;
+		DayLookupMap.lookupMap.put(value, this);
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public static Day get(int value) {
+		return DayLookupMap.lookupMap.get(value);
+	}
+
+	private static final class DayLookupMap {
+		private static final Map<Integer, Day> lookupMap = new HashMap<>();
+	}
+}
