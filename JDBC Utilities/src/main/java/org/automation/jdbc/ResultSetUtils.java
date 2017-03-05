419b71e18c0720a013077b7bc08f69fbad2b15f8
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-03-04 23:59:45 -0500

    replaced with blog item
    result set quarry should send the blog object

--- JDBC Utilities/src/main/java/org/automation/jdbc/ResultSetUtils.java
+++ JDBC Utilities/src/main/java/org/automation/jdbc/ResultSetUtils.java
@@ -54,7 +54,7 @@ public class ResultSetUtils {
   public static void savefile(ResultSet rs, final String file){
     int BUFFER_SIZE = 50000;
         File m_file = new File(file);
-        Blob blob = Step_Identify_Broadcast_Weeks_Days.getMediaFile(propid.toString());
+        Blob blob = rs.getBlob(1);
         InputStream inputStream  =blob.getBinaryStream();
         OutputStream outputStream = new FileOutputStream(m_file);
         int bytes = -1;
