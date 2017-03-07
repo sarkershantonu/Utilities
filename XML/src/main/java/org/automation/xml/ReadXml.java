968e9d220eaeb9cd66752c291c3cfa7cbe73e5b6
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-03-06 23:58:56 -0500

    package

--- XML/src/main/java/org/automation/xml/ReadXml.java
+++ XML/src/main/java/org/automation/xml/ReadXml.java
@@ -1,1 +1,12 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
 public class ReadXml {}
