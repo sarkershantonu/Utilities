4c499fd3bee951b6338a7ecfcf3b1f071d16e841
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-03-07 00:01:29 -0500

    validXml

--- XML/src/main/java/org/automation/xml/ReadXml.java
+++ XML/src/main/java/org/automation/xml/ReadXml.java
@@ -11,4 +11,20 @@ import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
 
-public class ReadXml {}
+public class ReadXml {
+public static void validXml(String pathToXSD, String pathToXml){
+        Source xmlFile = new StreamSource(new File(pathToXml));
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = null;
+        try {
+            schema = schemaFactory.newSchema(new File(pathToXSD));
+            Validator validator = schema.newValidator();
+            validator.validate(xmlFile);
+            System.out.println(xmlFile.getSystemId() + " is valid");
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
