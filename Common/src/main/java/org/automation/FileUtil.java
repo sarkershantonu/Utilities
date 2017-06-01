7e8b75569b11826b1477c4f8b7653be0fb882be4
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-05-31 22:15:50 -0400

    Update FileUtil.java

--- Common/src/main/java/org/automation/FileUtil.java
+++ Common/src/main/java/org/automation/FileUtil.java
@@ -1,1 +1,12 @@
 
+public class FileUtil {
+    /**
+     * This converts java path format to windows
+     * @param path_generic = Property has path with current workspace ./
+     * @return
+     */
+    public static String getWindowsFormatPath(String path_generic){
+        String path = new File(path_generic).getAbsolutePath();
+        return path.replace("/", "\\\\");
+    }
+}
