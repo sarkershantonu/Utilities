aaba1e813e56cbe748d3eb79ff42b534e7af8e03
sarkershantonu <shantonu_oxford@yahoo.com>
2017-06-01 22:33:49 -0400

    file util removed

--- Common/src/main/java/org/automation/FileUtil.java
+++ Common/src/main/java/org/automation/FileUtil.java
@@ -9,8 +9,5 @@ public class FileUtil {
      * @param path_generic = Property has path with current workspace ./
      * @return
      */
-    public static String getWindowsFormatPath(String path_generic){
-        String path = new File(path_generic).getAbsolutePath();
-        return path.replace("/", "\\\\");
-    }
+
 }
