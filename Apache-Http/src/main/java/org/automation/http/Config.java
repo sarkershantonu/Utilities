8bd35f395e09e44003f35113d1dfb332d115f9eb
sarkershantonu <shantonu_oxford@yahoo.com>
2017-06-02 23:54:32 -0400

    new value added

--- Apache-Http/src/main/java/org/automation/http/Config.java
+++ Apache-Http/src/main/java/org/automation/http/Config.java
@@ -11,4 +11,16 @@ public class Config {
     public static final String ENV = "";
 
     public static final String PCS_WS_URL = "";
+
+    public class FIRST_USER {
+        private static String name;
+        private static String password;
+        public static String getName() {
+            return name;
+        }
+
+        public static String getPassword() {
+            return password;
+        }
+    }
 }
