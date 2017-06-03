41b7b05787067a2d9b5188e1d4163f493271c99f
sarkershantonu <shantonu_oxford@yahoo.com>
2017-06-03 05:20:35 -0400

    xml document to stream

--- Apache-Http/src/main/java/org/automation/http/utils/XMLUtils.java
+++ Apache-Http/src/main/java/org/automation/http/utils/XMLUtils.java
@@ -2,14 +2,26 @@ package org.automation.http.utils;
 
 import org.automation.http.exception.SoapException;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * Created by shantonu on 6/3/17.
  */
 public class XMLUtils {
-    public static Document parseFileStream2XmlDocument(InputStream content) throws SoapException{
-
+    public static Document parseFileStream2XmlDocument(InputStream content) throws SoapException {
+        Document parsed = null;
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        try {
+            factory.newDocumentBuilder().parse(content);
+        } catch (Exception e) {
+            throw new SoapException("Error converting input stream to document >> " + e);
+        }
+        return parsed;
     }
 }
