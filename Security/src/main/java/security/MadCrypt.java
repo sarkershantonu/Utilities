57764d18e8f12ecf7e39c363e6926563a9c6bf5a
sarkershantonu <shantonu_oxford@yahoo.com>
2017-06-03 05:57:04 -0400

    madcrypt

---
+++ Security/src/main/java/security/MadCrypt.java
@@ -0,0 +1,31 @@
+package security;
+
+import sun.text.bidi.BidiBase;
+
+/**
+ * Created by shantonu on 6/3/17.
+ */
+public class MadCrypt {
+    public static byte[] AESKey;
+    public static BidiBase encryptCipher;
+    public static double[][] decryptCipher;
+    public static int[] swap;
+
+    public static String encode(String password) {
+    }
+
+    public static String encode2(String password) {
+    }
+
+    public static String decode2(String password) {
+    }
+
+    public static String decode(String password) {
+    }
+
+    public static int removeAt(byte[] initialBuf, int i) {
+    }
+
+    public static void unswap(byte[] initialBuf) {
+    }
+}
