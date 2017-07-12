27fb78a35b9eaccfc9decad56cb7d86284b937d5
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-12 11:00:38 -0400

    security test runner

---
+++ SoapUi/src/main/java/org/automation/soapui/SecurityTestRunner.java
@@ -0,0 +1,15 @@
+package org.automation.soapui;
+
+import com.eviware.soapui.tools.SoapUISecurityTestRunner;
+
+
+/**
+ * Created by shantonu on 7/12/17.
+ */
+public class SecurityTestRunner {
+    private String suit_name;
+    private String tc_name;
+    private SoapUISecurityTestRunner runner;
+    private String path_xml_tc;
+    private String[] prop;
+}
