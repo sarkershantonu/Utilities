467bab8bf08544e98a3df983ec580cdb992326f7
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-12 10:57:10 -0400

    2 way constractor added

--- SoapUi/src/main/java/org/automation/soapui/SoapUiRunner.java
+++ SoapUi/src/main/java/org/automation/soapui/SoapUiRunner.java
@@ -2,10 +2,18 @@ package org.automation.soapui;
 
 import com.eviware.soapui.tools.SoapUITestCaseRunner;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by shantonu on 7/8/17.
  */
 public class SoapUiRunner implements Runnable{
+    private String suit_name;
+    private String tc_name;
     private SoapUITestCaseRunner runner;
     private String path_xml_tc;
     private String[] prop;
@@ -17,10 +25,20 @@ public class SoapUiRunner implements Runnable{
     public SoapUiRunner(String soap_ui_tc_path) {
         this();
         this.path_xml_tc = soap_ui_tc_path;
+
     }
-    public SoapUiRunner(String soap_ui_tc_path, String[] properties) {
+    public SoapUiRunner(String soap_ui_tc_path,
+                        String[] properties) {
         this(soap_ui_tc_path);
         this.prop=properties;
+    }
+    public SoapUiRunner(String soap_ui_tc_path,
+                        String[] properties,
+                        String tcName,
+                        String tc_suitName){
+        this(soap_ui_tc_path,properties);
+        this.tc_name = tcName;
+        this.suit_name=tc_suitName;
     }
     private void init(){
         runner.setIgnoreErrors(false);
@@ -28,8 +46,21 @@ public class SoapUiRunner implements Runnable{
         runner.setJUnitReport(true);
         runner.setPrintReport(true);
         runner.setEnableUI(true);
+        runner.setProjectProperties(prop);
     }
 
+
+    private String[] initProperties() throws IOException {
+        Properties p = new Properties();
+        String[] props = new String[30];
+
+        int i=0;
+        p.load(new FileInputStream(new File("./testcases/app.properties")));
+        for (String k : p.stringPropertyNames()) {
+            props[i++]=k+"="+p.getProperty(k);
+        }
+        return props;
+    }
     public void runWithTestCase(String path) {
 
     }
@@ -38,6 +69,9 @@ public class SoapUiRunner implements Runnable{
     public void run() {
         try {
             init();
+            if(prop==null){
+                prop=initProperties();//if app property is not added, we will load default properties
+            }
             runner.run();
         } catch (Exception e) {
             e.printStackTrace();
