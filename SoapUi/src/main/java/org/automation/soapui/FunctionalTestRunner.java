ec4d31e844c7527c85979b0fdc31087e638a209e
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-12 10:58:43 -0400

    functional test case runner

--- SoapUi/src/main/java/org/automation/soapui/FunctionalTestRunner.java
+++ SoapUi/src/main/java/org/automation/soapui/FunctionalTestRunner.java
@@ -4,40 +4,39 @@ import com.eviware.soapui.tools.SoapUITestCaseRunner;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 /**
  * Created by shantonu on 7/8/17.
  */
-public class SoapUiRunner implements Runnable{
+public class FunctionalTestRunner implements Runnable{
     private String suit_name;
     private String tc_name;
     private SoapUITestCaseRunner runner;
     private String path_xml_tc;
     private String[] prop;
-    private SoapUiRunner(){
+    private FunctionalTestRunner(){
         runner = new SoapUITestCaseRunner();
 
     }
 
-    public SoapUiRunner(String soap_ui_tc_path) {
+    public FunctionalTestRunner(String soap_ui_tc_path) {
         this();
         this.path_xml_tc = soap_ui_tc_path;
 
     }
-    public SoapUiRunner(String soap_ui_tc_path,
-                        String[] properties) {
+    public FunctionalTestRunner(String soap_ui_tc_path,
+                                String[] properties) {
         this(soap_ui_tc_path);
         this.prop=properties;
         this.tc_name = "Default Test case name";
         this.suit_name="Default Test suit name";
     }
-    public SoapUiRunner(String soap_ui_tc_path,
-                        String[] properties,
-                        String tcName,
-                        String tc_suitName){
+    public FunctionalTestRunner(String soap_ui_tc_path,
+                                String[] properties,
+                                String tcName,
+                                String tc_suitName){
         this(soap_ui_tc_path,properties);
         this.tc_name = tcName;
         this.suit_name=tc_suitName;
