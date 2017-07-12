3e2d91723628de096c622e520187d599d94c5b9e
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-12 10:59:55 -0400

    load test runner

---
+++ SoapUi/src/main/java/org/automation/soapui/PerformanceTestRunner.java
@@ -0,0 +1,15 @@
+package org.automation.soapui;
+
+import com.eviware.soapui.tools.SoapUILoadTestRunner;
+import com.eviware.soapui.tools.SoapUITestCaseRunner;
+
+/**
+ * Created by shantonu on 7/12/17.
+ */
+public class PerformanceTestRunner {
+    private String suit_name;
+    private String tc_name;
+    private SoapUILoadTestRunner runner;
+    private String path_xml_tc;
+    private String[] prop;
+}
