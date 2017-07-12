2f4608fddd7dbb4f0cb097deae705453a03b189a
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-12 10:27:12 -0400

    runner for funciton soap UI tests

--- SoapUi/src/main/java/org/automation/soapui/SoapUiRunner.java
+++ SoapUi/src/main/java/org/automation/soapui/SoapUiRunner.java
@@ -8,14 +8,26 @@ import com.eviware.soapui.tools.SoapUITestCaseRunner;
 public class SoapUiRunner implements Runnable{
     private SoapUITestCaseRunner runner;
     private String path_xml_tc;
-
+    private String[] prop;
     private SoapUiRunner(){
         runner = new SoapUITestCaseRunner();
+
     }
 
-    private SoapUiRunner(String soap_ui_tc_path) {
+    public SoapUiRunner(String soap_ui_tc_path) {
         this();
         this.path_xml_tc = soap_ui_tc_path;
+    }
+    public SoapUiRunner(String soap_ui_tc_path, String[] properties) {
+        this(soap_ui_tc_path);
+        this.prop=properties;
+    }
+    private void init(){
+        runner.setIgnoreErrors(false);
+        runner.setExportAll(true);
+        runner.setJUnitReport(true);
+        runner.setPrintReport(true);
+        runner.setEnableUI(true);
     }
 
     public void runWithTestCase(String path) {
@@ -25,6 +37,7 @@ public class SoapUiRunner implements Runnable{
     @Override
     public void run() {
         try {
+            init();
             runner.run();
         } catch (Exception e) {
             e.printStackTrace();
